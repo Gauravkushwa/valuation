4fa93ce873e3d36ff96018c46eb24f069f1f0ecb
Gaurav Kushwaha <kushwahagaurav062@gmail.com>
2024-06-02 17:47:48 +0530

    ReactEva commit

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -2,14 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login"
 import UserDetails from "../pages/UserDetails";
-import Products from "../pages/Products";
 
 export default function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/products" element={<Products />} />
       <Route path="/users/:user_id" element={<UserDetails />} />
     </Routes>
   );
